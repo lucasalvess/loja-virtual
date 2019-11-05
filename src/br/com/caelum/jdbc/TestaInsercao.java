6017c12bf0c4d9496b72b8a7500780460ed56490
Lucas Alves da silva <lcluphus@gmail.com>
2019-11-05 19:28:30 -0300

    insere-com-prepared-statement
    Adiciona o escape das strings usando o prepare statement

--- src/br/com/caelum/jdbc/TestaInsercao.java
+++ src/br/com/caelum/jdbc/TestaInsercao.java
@@ -2,6 +2,7 @@ package br.com.caelum.jdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -12,11 +13,18 @@ public class TestaInsercao {
 
 	 public static void main(String[] args) throws SQLException {
 		 
+		String nome = "Produto tal";
+		String descricao = "Um produto com tais caracteristicas";
+		 
 		Connection conexao = Conexao.conecta();
 		
-		Statement statement = conexao.createStatement();
+		String sql = "insert into produto(nome,descricao) values(?,?)";
+		PreparedStatement statement = conexao.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		statement.setString(1, nome);
+		statement.setString(2, descricao);
 		
-		boolean resultado = statement.execute("insert into produto(nome,descricao) values('Teste','um teste')",statement.RETURN_GENERATED_KEYS);
+		 
+		boolean resultado = statement.execute();
 		
 		ResultSet rs = statement.getGeneratedKeys();
 		
@@ -24,7 +32,7 @@ public class TestaInsercao {
 			System.out.println("Gerou o id: "+rs.getString("id"));
 		}
 		
-		
+		rs.close();
 		statement.close();
 		conexao.close();
 	}
